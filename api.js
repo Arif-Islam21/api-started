de64ea56326fe5cd15a75a6078d3dd82b31b9e09
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-26 19:45:59 +0600

    learning stringify

--- api.js
+++ api.js
@@ -1,1 +1,5 @@
-console.log("hello from API world");
+// console.log("hello from API world");
+const user = { id: 1, name: "Gorib Amir", job: "actor" };
+const stringified = JSON.stringify(user);
+console.log(stringified);
+console.log(user);
