f93f9c17e6283aa7bcb294eabbdca6ef5d946950
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-26 19:49:51 +0600

    complex JSON

--- api.js
+++ api.js
@@ -1,5 +1,18 @@
 // console.log("hello from API world");
 const user = { id: 1, name: "Gorib Amir", job: "actor" };
 const stringified = JSON.stringify(user);
-console.log(stringified);
-console.log(user);
+// console.log(stringified);
+// console.log(user);
+
+const shop = {
+  owner: "Alia",
+  address: {
+    street: "Kochukhet ,Vuter Goli",
+    city: "ranbir",
+    country: "BD",
+  },
+  product: ["laptop", "mic", "monitor", "keyboard"],
+  revenue: 45000,
+  isOpen: true,
+  isNew: false,
+};
