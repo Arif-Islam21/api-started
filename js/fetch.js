e7b82ae647c7cf398d02d433618bae683e8af8fe
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-28 15:23:22 +0600

    loading users

--- js/fetch.js
+++ js/fetch.js
@@ -7,9 +7,9 @@
 // );
 //   .then((json) => console.log(json));
 
-function loadData() {
-  const url = "https://jsonplaceholder.typicode.com/todos/1";
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
+// function loadData() {
+//   const url = "https://jsonplaceholder.typicode.com/todos/1";
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// }
